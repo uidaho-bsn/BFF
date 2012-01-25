310d763bbd8f46c2b6201d780c37f287c5445576
Max Stillwell <mstillwell@vandals.uidaho.edu>
2012-01-25 15:04:39 -0800

    Ruby on Rails!

---
+++ tab/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tab::Application.config.secret_token = 'c871f031744dca57948db894597b7c1d40914990780d562ec2d98821b9d3283b21c91918e5c9a03d85c2a1a4be3064bf334ee4972c5f37ba1a027a66098903e0'
