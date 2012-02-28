60a514653ca3c1b32922663f4f084908ac20882c
Sasha Solomon <lndndark@gmail.com>
2012-02-27 16:28:39 -0800

    added functionality for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,8 +7,9 @@ class UserMailer < ActionMailer::Base
   #   en.user_mailer.password_reset.subject
   #
   def password_reset
-    @greeting = "Hi"
+    @user = user
 
-    mail to: "to@example.org"
+
+    mail :to => user.email, :subject => "Password Reset"
   end
 end
