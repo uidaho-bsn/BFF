7d614ecf66a169c311f1a6fa142a5d1aa31fed64
Sasha Solomon <lndndark@gmail.com>
2012-03-22 23:53:24 -0700

    mailer is now functional on local server. made changes to make mailer functional on heroku. will test on heroku.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,15 +46,10 @@ Tab::Application.configure do
   
   config.action_mailer.delivery_method = :sendmail
 
-
   config.action_mailer.perform_deliveries = true
+  
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => '<bassoonfingeringfinder@gmail.com>',
-    :password             => '<Bassoon11>',
-    :authentication       => :login,
-    :enable_starttls_auto => true  }
+
+
+  config.consider_all_requests_local = true
 end
