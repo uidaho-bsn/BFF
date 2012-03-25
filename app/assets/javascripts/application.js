6c3bd8ccecbd30b7584b1eab45d1c6c7c04db5b1
Max Stillwell <mstillwell@vandals.uidaho.edu>
2012-03-25 16:17:53 -0700

    Removed some jQuery stuff I don't need.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,15 +13,3 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-
-jQuery.ajaxSetup({ 
-  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
-})
-
-jQuery.fn.submitWithAjax = function() {
-  this.submit(function() {
-    $.post(this.action, $(this).serialize(), null, "script");
-    return false;
-  })
-  return this;
-};
