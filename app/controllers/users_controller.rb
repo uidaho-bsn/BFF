9660fd31db7806df97e1cebf8f1b9ff164067d76
Max Stillwell <mstillwell@vandals.uidaho.edu>
2012-03-27 17:25:45 -0700

    Fixed a notice.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
       redirect_to root_url
       else
         flash[:warning] = "Login Failed"
-        redirect_to root_url
+        redirect_to root_url, :notice => "Login Failed"
       end
     end
   end
