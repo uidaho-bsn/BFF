fa6b29f8a42a088dcbde03e97fd32ee0b4108325
Max Stillwell <mstillwell@vandals.uidaho.edu>
2012-04-14 14:06:11 -0700

    Validation fix.

--- app/assets/javascripts/Validation_Edit.js
+++ app/assets/javascripts/Validation_Edit.js
@@ -12,12 +12,22 @@ $(document).ready(function () {
 			"user[email]": {
 				required: true, 
 				email: true, 
+			},
+			"user[password]": {
+				required: true, 
+				minlength: 6,
+				maxlength: 64,
 			}
 		},
 		messages: {
 			"user[email]": {
 				required: "Email is required!", 
 				email: "Not a valid email address!", 
+			},
+			"user[password]": {
+				required: "Password is required!", 
+				minlength: "Password must be longer than 6 characters!",
+				maxlength: "Password must be shorter than 64 characters!",
 			}
 		}
 	});
