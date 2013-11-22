df3172f7e3dd54e4d03e0492c14f14e23e2df193
Pierce Trey <cellomanpierce@gmail.com>
2013-11-21 16:48:36 -0800

    Trying to get precompiling to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Tab::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( *.js *.css )
   # config.assets.precompile = true
 
   # Disable delivery errors, bad email addresses will be ignored
